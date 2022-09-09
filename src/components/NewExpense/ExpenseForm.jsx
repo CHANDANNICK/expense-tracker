6b064e314e1e6815a073e4e2cf9981f3ec9f6381
CHANDANNICK <Chandannick@gmail.com>
2022-09-09 11:36:07 +0530

    Creating New Expense Form

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import '../../assets/css/ExpenseForm.css';
 
 const ExpenseForm = () => {
+	const titleChangeHandler = (event) => {
+		console.log(event.target.value);
+	};
+
 	return (
 		<form>
-			<div className='expense-form__controls'>
-				<div className='expense-form__control'>
+			<div className='new-expense__controls'>
+				<div className='new-expense__control'>
 					<label htmlFor='title'>Title</label>
-					<input type='text' />
+					<input type='text' onChange={titleChangeHandler} />
 				</div>
-				<div className='expense-form__control'>
+				<div className='new-expense__control'>
 					<label htmlFor='amount'>Amount</label>
 					<input type='number' />
 				</div>
-				<div className='expense-form__control'>
+				<div className='new-expense__control'>
 					<label htmlFor='date'>Date</label>
 					<input type='date' />
 				</div>
 			</div>
-			<div className='expense-form__actions'>
-				<button type='submit' className='expense-form__action'>
+			<div className='new-expense__actions'>
+				<button type='submit' className='new-expense__action'>
 					Submit
 				</button>
 			</div>
